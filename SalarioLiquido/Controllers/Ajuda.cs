f21068895b0a451de686840f10a4a5ff7eb6c841
RicardoPereiraRS@github.com <jose_i69@hotmail.com>
2020-08-24 17:52:12 -0300

    removido usings não utilizados

--- SalarioLiquido/Controllers/Ajuda.cs
+++ SalarioLiquido/Controllers/Ajuda.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace SalarioLiquido.Controllers
 {
-	[Route("[controller]")]
+    [Route("[controller]")]
 	[ApiController]
 	public class Ajuda : ControllerBase	
 	{
